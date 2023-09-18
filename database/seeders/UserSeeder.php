b54b90c30ed9513950ce7d3e105d94286056fca4
Nihat Avcı <nihatavci91@gmail.com>
2023-09-18 20:55:06 +0300

    düzelti.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,14 +14,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        /*
         User::create([
             'name' => 'nihat.avci',
             'email' => 'nihat@test.com',
             'is_admin' => 1,
             'password' => bcrypt('12345678'),
         ]);
+        */
         User::create([
-            'name' => 'nihat.avci',
+            'name' => 'bienal',
             'email' => 'info@bienal.com.tr',
             'is_admin' => 1,
             'password' => bcrypt('bienal18092023'),
