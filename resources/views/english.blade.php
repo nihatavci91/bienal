4e50285baa58bdc9255533496e9ca75cae1289b2
Nihat Avcı <nihatavci91@gmail.com>
2024-01-11 22:48:16 +0300

    düzelti.

--- resources/views/english.blade.php
+++ resources/views/english.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index">
+    <meta name="description" content="Bienal">
+    <meta name="viewport" content="Bienal Ajans">
+    <meta name="rating" content="adult">
     <title>Bienal</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://unpkg.com/splitting/dist/splitting.min.js"></script>
