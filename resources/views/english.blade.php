d9d78d631aad0e1f5efdfb396ba33168be097899
Nihat Avcı <nihatavci91@gmail.com>
2024-01-29 23:08:25 +0300

    düzelti.

--- resources/views/english.blade.php
+++ resources/views/english.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index">
     <meta name="description" content="Bienal">
-    <meta name="viewport" content="Bienal Ajans">
     <meta name="rating" content="adult">
     <title>Bienal</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
